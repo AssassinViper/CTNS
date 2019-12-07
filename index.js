5090746a014e257e15d23b5c018ebd537d5f8a45
AssassinViper <amd.amp1969@gmail.com>
2019-12-07 21:01:04 +0330

    favicon

--- index.js
+++ index.js
@@ -107,6 +107,11 @@ app.post(Routes.ADMIN_EDIT_WEEKLY, upload_temp.single('file'), AdminAuth, AdminH
 
 ////////////////////////////////////////////////////////////////////
 
+app.get("/favicon.ico", (req,res)=>{
+
+    res.sendFile(path.join(__dirname+ "/client/build/favicon.ico"));
+})
+
 app.get("*", (req, res)=>{
 
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
